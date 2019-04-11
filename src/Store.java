dfdca5d709aefa14fdf9d2ef216f65c0119f3fc2
Darituan <44865730+Darituan@users.noreply.github.com>
2019-04-11 16:42:45 +0300

    Update Store.java

--- src/Store.java
+++ src/Store.java
@@ -22,6 +22,26 @@ public class Store {
         return departments.get(index);
     }
 
+    public Department getDepartment(String name){
+        for (Department department:departments){
+            if (department.equals(name)) return department;
+        }
+        return null;
+    }
+
+    public int totalPrice(){
+        int[] totalPrice = {0};
+        departments.forEach(department -> totalPrice[0]+=department.groupPrice());
+        return totalPrice[0];
+    }
+
+    public ArrayList<Product> getAllProducts(){
+        ArrayList<Product> products = new ArrayList<>();
+        departments.forEach(department -> products.addAll(department.getProducts()));
+        if (products.size()==0)return null;
+        return products;
+    }
+
     public String toString() {
         String s = "";
         for (int i = 0; i < departments.size(); i++) {
