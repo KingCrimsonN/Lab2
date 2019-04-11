8286af4707c360dd59f892c1d55e892cc3ca0239
KingCrimsonN <48062712+KingCrimsonN@users.noreply.github.com>
2019-04-11 15:12:24 +0300

    Update Store.java

--- src/Store.java
+++ src/Store.java
@@ -3,25 +3,29 @@ import java.util.ArrayList;
 public class Store {
     ArrayList<Department> departments = new ArrayList<>();
 
-    public void add(Department dep){
-        departments.add(dep);
+    public void add(String name) {
+        departments.add(new Department(name));
     }
-    public void edit(Department dep, String name){
-        for (int i=0; i<departments.size(); i++){
+
+    public void edit(Department dep, String name) {
+        for (int i = 0; i < departments.size(); i++) {
             if (departments.get(i).equals(dep.name))
                 departments.get(i).name = name;
         }
     }
-    public void remove(Department dep){
+
+    public void remove(Department dep) {
         departments.remove(dep);
     }
-    public Department getDepartment(int index){
+
+    public Department getDepartment(int index) {
         return departments.get(index);
     }
-    public String toString(){
-        String s="";
-        for (int i=0; i<departments.size(); i++){
-            s+=departments.get(i).toString();
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < departments.size(); i++) {
+            s += departments.get(i).toString();
         }
         return s;
     }
