330e10c5a86b6ebbf31c99b072bbd6c38a308090
Darituan <44865730+Darituan@users.noreply.github.com>
2019-04-11 14:14:41 +0300

    Update SearchUtilit.java

--- src/SearchUtilit.java
+++ src/SearchUtilit.java
@@ -1,17 +1,17 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SearchUtilit {
-
+    
+    @SuppressWarnings("all")
     public static ArrayList findByName(ArrayList list, String namePart){
-        ArrayList found = new ArrayList();
-        list.forEach(element -> {
-            if (element instanceof Product){
-                if (((Product) element).getName().contains(namePart))
-                    found.add(element);
-            }
-            else if (((Department)element).getName().contains(namePart))
-                found.add(element);
-        });
+        if (list==null)return null;
+        if (list.size()==0)return null;
+        List foundList = (List)list.stream().filter(element ->
+                ((element instanceof Product)&&((Product) element).getName().contains(namePart))||
+                        (element instanceof Department&&((Department)element).getName().contains(namePart))).collect(Collectors.toList());
+        ArrayList found = new ArrayList(foundList);
         if (found.size()==0)return null;
         // I'm not sure if automatic sort of search result by the same parameter they were found by is needed
         // maybe I'll have to DELET THIS
@@ -36,14 +36,11 @@ public class SearchUtilit {
     }
 
     private static ArrayList<Product> findProdByMoney(ArrayList<Product> allProducts, int lowest, int highest, boolean byValue){
-        ArrayList<Product> foundProducts = new ArrayList<>();
-        allProducts.forEach(product -> {
-            int valOrPrice;
-            if (byValue) valOrPrice = product.getValue();
-            else valOrPrice = product.getPrice();
-            if(valOrPrice>=lowest&&valOrPrice<=highest)
-                foundProducts.add(product);
-        });
+        if (allProducts==null)return null;
+        ArrayList<Product> foundProducts = new ArrayList<>(allProducts.stream().filter(
+                product -> (byValue&&product.getValue()>=lowest&&product.getValue()<=highest)||
+                        (!byValue&&product.getPrice()>=lowest&&product.getPrice()<=highest)
+        ).collect(Collectors.toList()));
         if(foundProducts.size()==0)return null;
         // I'm not sure if automatic sort of search result by the same parameter they were found by is needed
         // maybe I'll have to DELET THIS
@@ -52,19 +49,16 @@ public class SearchUtilit {
     }
 
     private static ArrayList<Product> findProdByMoney(ArrayList<Product> allProducts, int amountOfMoney, boolean byValue){
-        ArrayList<Product> foundProducts = new ArrayList<>();
-        allProducts.forEach(product -> {
-            int valOrPrice;
-            if (byValue) valOrPrice = product.getValue();
-            else valOrPrice = product.getPrice();
-            if(valOrPrice==amountOfMoney)
-                foundProducts.add(product);
-        });
+        if (allProducts==null)return null;
+        ArrayList<Product> foundProducts = new ArrayList<>(allProducts.stream().filter(
+                product -> (byValue&&product.getValue()==amountOfMoney)||
+                        (!byValue&&product.getPrice()==amountOfMoney)
+        ).collect(Collectors.toList()));
         if(foundProducts.size()==0)return null;
         // I'm not sure if automatic sort of search result by the same parameter they were found by is needed
         // maybe I'll have to DELET THIS
-        SortUtilit.sortByMoney(foundProducts,byValue);
+        SortUtilit.sortByName(foundProducts);
         return foundProducts;
     }
-
+    
 }
