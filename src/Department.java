641ddf455f886fafb214706f438f70470af539ae
KingCrimsonN <48062712+KingCrimsonN@users.noreply.github.com>
2019-04-15 18:37:24 +0300

    Updated Department

--- src/Department.java
+++ src/Department.java
@@ -9,15 +9,16 @@ public class Department {
         name = nName;
     }
 
-    public void add(String nName, int am, int price) {
-        products.add(new Product(nName, am, price));
+    public void add(String nName, int am, int price, String image) {
+        if (checkUnique(nName))
+            products.add(new Product(nName, am, price,image));
     }
 
-    public void edit(Product prod, String name, int val, int price) {
+    public void edit(Product prod, String name, int val, int price, String img) {
         if (checkUnique(name))
             for (int i = 0; i < products.size(); i++) {
                 if (prod.name.equals(products.get(i)))
-                    products.get(i).edit(name, val, price);
+                    products.get(i).edit(name, val, price,img);
             }
     }
 
@@ -25,12 +26,15 @@ public class Department {
         products.remove(prod);
     }
 
-    public String toString() {
+    public String toSaveString() {
         String s = "/g " + name + "\n";
         for (int i = 0; i < products.size(); i++) {
             s += products.get(i).toString();
         }
         return s;
+    }
+    public String toString(){
+        return name;
     }
 
     public int groupPrice() {
